a7a6a997250369af3cb45ae91106cfb9364ddae9
xychong <xychong1711@gmail.com>
2021-12-21 11:14:34 +0800

    Added new dimension to input feature

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -78,6 +78,7 @@ while True:
         # load audio file and extract features
         if path.exists(WAV_PATH + row[1]):
             predict_x = extract_features_only(WAV_PATH + row[1])
+            predict_x = np.expand_dims(predict_x, axis=0)
             interpreter.set_tensor(input_details[0]['index'], predict_x.astype(np.uint8))
             #input_details = interpreter.get_input_details()[0] # for one input data
             #tensor_index = input_details['index'] # tensor index in the interpreter
