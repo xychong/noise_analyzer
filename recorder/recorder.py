ca72b180934b4f0051977003ce0df3af9c0689a6
xychong <xychong1711@gmail.com>
2022-01-02 16:05:40 +0800

    changed print statement

--- recorder/recorder.py
+++ recorder/recorder.py
@@ -185,11 +185,11 @@ def listen_for_speech(threshold=THRESHOLD):
             prev_audio.append(cur_data) # prepend previous audio to current chunk; do this for each chunk 
             #print("prev_audio length: ", len(prev_audio))
 
-    print("Finished recording.")
+    print("Finished.")
     stream.close() # stop stream
     p.terminate() # close PyAudio
 
-    return response
+    #return response
 
 
 def save_speech(data, p):
