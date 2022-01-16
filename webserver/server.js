c91e04dab654fe29f74fe8f95815ee0fcc2e2867
xychong <xychong1711@gmail.com>
2022-01-16 16:28:43 +0800

    generate downloadable link

--- webserver/server.js
+++ webserver/server.js
@@ -314,7 +314,6 @@ async function buildExport(req) {
       }  // end for
       //my_table = my_table + "]  }"
     resolve(my_table);
-    download('noisedata.txt', my_table);
     });  // end db
   });  // end promise
 }
@@ -348,18 +347,6 @@ function buildExportJSON(row) {
     });
 }
 
-function download(filename, text) {
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
-
-  element.style.display = 'none';
-  document.body.appendChild(element);
-
-  element.click();
-
-  document.body.removeChild(element);
-}
 
 // --------------
 
