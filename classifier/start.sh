d405d1e5aa3cdb56b6471b8dfb1af995ed3df75f
xychong <xychong1711@gmail.com>
2022-01-21 11:41:48 +0800

    added new model path

--- classifier/start.sh
+++ classifier/start.sh
@@ -6,6 +6,7 @@ sleep 2
 cp /usr/src/app/mobilenet_v2_sound_classification_qat_edgetpu.tflite /data/sound_app/mobilenet_v2_sound_classification_qat_edgetpu.tflite
 cp /usr/src/app/mobilenet_v2_sound_classification_float_qat_edgetpu.tflite /data/sound_app/mobilenet_v2_sound_classification_float_qat_edgetpu.tflite
 cp /usr/src/app/mobilenet_v2_sound_classification_ptq_edgetpu.tflite /data/sound_app/mobilenet_v2_sound_classification_ptq_edgetpu.tflite
+cp /usr/src/app/mobilenet_v2_sound_classification_float_ptq_edgetpu.tflite /data/sound_app/mobilenet_v2_sound_classification_float_ptq_edgetpu.tflite
 cp /usr/src/app/class_labels.txt /data/sound_app/class_labels.txt
 
 # Start the classifier
